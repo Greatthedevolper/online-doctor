630ffd24fab73130e2790840c6f9cd54359d38af
Azeem <greatthedevolper@gmail.com>
2023-09-20 02:06:06 +0500

    PERSCRIPTION STEP FORM
    APPEND CHAT MSG WORK ON STEP FORM

--- js/main.js
+++ js/main.js
@@ -3,8 +3,25 @@ $(document).ready(function () {
     $(".tab-button").removeClass("active");
     $(this).addClass("active");
   });
-});
+  $(".compile-btn").click(function () {
+    $(".compile-wrapper").toggleClass("active");
+    $(".inner-div").toggleClass("active");
+    
+  });
 
+  $(".close-right-sidebar").click(function () {
+    showLeftSidebar();
+  });
+  function showLeftSidebar() {
+    $(".right-sidebar").removeClass("active");
+    $(".left-sidebar").removeClass("active");
+  }
+  function hidwLeftSidebar() {
+    $(".right-sidebar").addClass("active");
+    $(".left-sidebar").addClass("active");
+  }
+});
+// doctor portfolio step form
 $(document).ready(function () {
   var form = $("#doctor-profile-form");
   var progressBar = $(".progress-bar");
@@ -41,8 +58,6 @@ $(document).ready(function () {
     }
   });
 
-  
-
   $(".prev").click(function () {
     var step = $(this).data("step");
     $(".doctor-profile-step").hide();
@@ -50,23 +65,14 @@ $(document).ready(function () {
     updateProgressBar(step);
   });
 
-  // function updateProgressBar(step) {
-  //   var progress = (step / totalSteps) * 100;
-  //   $(".doctor-profile-page").css("width", progress + "%");
-  //   progressBar.attr("aria-valuenow", progress);
-  //   $(".step-number").text(step); // Update the step number
-  //   $(".step-circle[]");
-  // }
-
   function updateProgressBar(step) {
     stepCircles.filter("[data-circle='" + step + "']").addClass("active");
     var progress = ((step - 1) / 2) * 100;
-    $(".progress-bar").css("width", progress + "%");
+    progressBar.css("width", progress + "%");
   }
 });
 
-
-// Right side bar js
+// step form form Right side step form bar js
 $(document).ready(function () {
   var currentStep = 1;
   var totalSteps = $(".prescription-step").length;
@@ -74,17 +80,11 @@ $(document).ready(function () {
   showStep(currentStep);
 
   $(".next-step").click(function () {
-    if (currentStep < totalSteps) {
-      currentStep++;
-      showStep(currentStep);
-    }
+    nextStep();
   });
 
   $(".prev-step").click(function () {
-    if (currentStep > 1) {
-      currentStep--;
-      showStep(currentStep);
-    }
+    preStep();
   });
 
   $("#prescription-form").submit(function (e) {
@@ -92,11 +92,156 @@ $(document).ready(function () {
     alert("Form submitted successfully!");
   });
 
-  // Function to show the current step and update the indicator
+  function preStep() {
+    if (currentStep > 1) {
+      currentStep--;
+      showStep(currentStep);
+    }
+  }
+
+  function nextStep() {
+    if (currentStep < totalSteps) {
+      currentStep++;
+      showStep(currentStep);
+    }
+  }
+
   function showStep(step) {
     $(".prescription-step").hide();
     $(".indicator-step").removeClass("active");
     $(".prescription-step:nth-child(" + step + ")").show();
     $(".indicator-step:nth-child(" + step + ")").addClass("active");
   }
+});
+
+$(document).ready(function () {
+  var bookStep = 1;
+  var totalBookSteps = $(".book-appoinment-step").length;
+  var appoinmentProgressBar = $(".appointment-wrapper .progress-bar");
+
+  showBookStep(bookStep);
+
+  $(".indicator-book-step").click(function () {
+    nextBookStep();
+  });
+  $(".next-book-step").click(function () {
+    nextBookStep();
+  });
+
+  // $(".indicator-book-step").click(function () {
+  //   preBookStep();
+  // });
+
+  $("#prescription-form").submit(function (e) {
+    e.preventDefault();
+  });
+
+  function preBookStep() {
+    if (bookStep > 1) {
+      bookStep--;
+      showBookStep(bookStep);
+    }
+  }
+
+  function nextBookStep() {
+    if (bookStep < totalBookSteps) {
+      bookStep++;
+      showBookStep(bookStep);
+      if (bookStep == 3) {
+        hidwLeftSidebar();
+      } else {
+        showLeftSidebar();
+        
+      }
+    }
+  }
+  function showLeftSidebar() {
+    $(".right-sidebar").removeClass("active");
+    $(".left-sidebar").removeClass("active");
+  }
+  function hidwLeftSidebar() {
+    $(".right-sidebar").addClass("active");
+    $(".left-sidebar").addClass("active");
+  }
+  function showBookStep(bookStep) {
+    $(".book-appoinment-step").hide();
+    // $(".indicator-book-step").removeClass("active");
+    $(".book-appoinment-step:nth-child(" + bookStep + ")").show();
+    $(".indicator-book-step:nth-child(" + bookStep + ")").addClass("active");
+
+    var bookProgress = ((bookStep - 1) / 3) * 100;
+    appoinmentProgressBar.css("width", bookProgress + "%");
+  }
+});
+
+$(document).ready(function () {
+  // Define a function to handle the chat message rendering
+  function renderChatMessage() {
+    let msg = $("#chat-input").val();
+    let renderMsg = document.createElement("li");
+    renderMsg.className = "chat-item doctor";
+    renderMsg.innerHTML = `<div class="img-wrapper">
+                    <img src="../images/doctor-profile.png" alt="img" />
+                  </div>
+                  <div class="content-wrapper">
+                    <p>${msg}</p>
+                    <small>11:41AM</small>
+                  </div>
+                  <div class="dropdown">
+                    <button
+                      class="dropdown-toggle"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <i class="fa-solid fa-chevron-down"></i>
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          <img
+                            src="../images/icons/reply-icon.svg"
+                            alt=""
+                          />
+                          <span>Reply</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          <img
+                            src="../images/icons/edit-2-icon.svg"
+                            alt=""
+                          />
+                          <span>Edit Message</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          <img
+                            src="../images/icons/trash-icon.svg"
+                            alt=""
+                          />
+                          <span>Delete for everyone</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>`;
+    $(".chat-item-wrapper").append(renderMsg);
+
+    $("#chat-input").val("");
+
+     $(".chat-body-wrapper").scrollTop($(".chat-body-wrapper")[0].scrollHeight);
+  }
+
+  $(document).ready(function () {
+    $(".input-wrapper").submit(function (event) {
+      event.preventDefault();
+      renderChatMessage();
+    });
+
+    // Trigger the same function on button click
+    $("#chat-input-btn").click(function () {
+      renderChatMessage();
+    });
+  });
 });
